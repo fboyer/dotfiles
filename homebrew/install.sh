a0dc770564cfca70ee96440718e735d7a1e6f654
fboyer <fboyer@gmail.com>
2014-03-08 13:35:56 -0500

    removed a few unused packages and adapted a few files

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,39 @@ then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
 fi
 
+# Make sure we have updated the formulas
+brew update
+brew upgrade
+
 # Install homebrew packages
-brew install grc coreutils spark
+brew install grc 
+brew install bazaar 
+brew install boot2docker
+brew install coreutils 
+brew install curl
+brew install dart 
+brew install docker
+brew install git --with-brewed-curl --with-brewed-openssl
+brew install go --cross-compile-common
+brew install mercurial
+brew install node --enable-debug
+brew install openconnect
+brew install spark 
+brew install wget
+brew install zsh
+
+# Install native apps
+brew install phinze/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install evernote
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install kindle
+brew cask install spectacle
+brew cask install the-unarchiver
+brew cask install flux
+brew cask install sublime-text3
+brew cask install vmware-fusion
 
 exit 0
