1083ff1b53f6d13b596de1d374c6ea80ce97b699
Frédéric Boyer <fboyer@gmail.com>
2014-03-14 18:37:33 -0400

    Updated dot, homebrew and osx to less aggresive initial settings due to weird behavior with old scripts.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,37 +13,12 @@ then
 fi
 
 # Install homebrew packages
-brew install grc 
-brew install bazaar 
-brew install boot2docker
-brew install coreutils 
-brew install curl
-brew install dart 
-brew install docker
-brew install git --with-brewed-curl --with-brewed-openssl
-brew install go --cross-compile-common
-brew install mercurial
-brew install node --enable-debug
-brew install openconnect
-brew install spark 
-brew install wget
-brew install zsh
+brew install grc bazaar boot2docker coreutils curl dart docker git --with-brewed-curl --with-brewed-openssl go --cross-compile-common mercurial node --enable-debug spark wget zsh
 
 # Install native apps
 brew install phinze/cask/brew-cask
 brew tap caskroom/versions
 
-brew cask install evernote
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install kindle
-brew cask install lastpass-universal
-brew cask install spectacle
-brew cask install the-unarchiver
-brew cask install flux
-brew cask install sublime-text3
-# brew cask install vagrant
-brew cask install virtualbox
-# brew cask install vmware-fusion
+brew cask install evernote google-chrome google-drive kindle lastpass-universal spectacle the-unarchiver flux sublime-text3 vagrant virtualbox vmware-fusion
 
 exit 0
